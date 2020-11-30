3936619041e9ffa669f86fa16e4c57ee1ee2e653
SourireG <819829381@qq.com>
2020-11-30 19:00:50 +0800

    ZHIHAN change 2

--- app/src/main/java/com/example/robot/map/AddNewMapFragment.java
+++ app/src/main/java/com/example/robot/map/AddNewMapFragment.java
@@ -2,7 +2,6 @@ package com.example.robot.map;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -12,25 +11,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 
 import com.example.robot.EmptyClient;
-import com.example.robot.MainActivity;
 import com.example.robot.R;
 import com.example.robot.SettingsActivity;
-import com.example.robot.content.Content;
 import com.example.robot.content.GsonUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class FirstFragment extends Fragment implements View.OnClickListener {
+public class AddNewMapFragment extends Fragment implements View.OnClickListener {
 
-    private static final String TAG = "FirstFragment";
+    private static final String TAG = "AddNewMapFragment";
 
-    @BindView(R.id.main_settings)
-    Button settingsButton;
 
 
 
@@ -63,7 +57,7 @@ public class FirstFragment extends Fragment implements View.OnClickListener {
     }
 
     private void initView() {
-        settingsButton.setOnClickListener(this);
+
     }
 
     private void initListener() {
@@ -74,10 +68,7 @@ public class FirstFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.main_settings:
-                Log.d(TAG, "onEventMsg ： " + "ssssss");
-                Intent intent = new Intent();
-                intent.setClass(getActivity(),SettingsActivity.class);
-                getActivity().startActivity(intent);
+
                 break;
             default:
                 break;
