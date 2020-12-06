6f3ab6e5a98a49e2b0165799710d38a878e9a334
SourireG <819829381@qq.com>
2020-12-06 18:12:00 +0800

    Zhihan change 12.6(3)

--- app/src/main/java/com/example/robot/map/TaskManagerFragment.java
+++ app/src/main/java/com/example/robot/map/TaskManagerFragment.java
@@ -1,10 +1,13 @@
 package com.example.robot.map;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,10 +24,12 @@ import com.bumptech.glide.Glide;
 import com.example.robot.EmptyClient;
 import com.example.robot.MainActivity;
 import com.example.robot.R;
+import com.example.robot.bean.DrawLineBean;
 import com.example.robot.content.Content;
 import com.example.robot.content.EventBusMessage;
 import com.example.robot.content.GsonUtils;
 import com.example.robot.task.TaskNewFragment;
+import com.example.robot.util.NormalDialogUtil;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
@@ -34,6 +39,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -52,6 +59,8 @@ public class TaskManagerFragment extends Fragment implements View.OnClickListene
     ImageView taskManageMapImage;
     @BindView(R.id.task_manage_map_relative)
     RelativeLayout taskManageMapRelative;
+    @BindView(R.id.task_manage_map_relative_border)
+    RelativeLayout taskManageMapRelativeBorder;
     @BindView(R.id.task_manage_edit)
     Button taskEdit;
     @BindView(R.id.task_manage_delete)
@@ -64,6 +73,13 @@ public class TaskManagerFragment extends Fragment implements View.OnClickListene
     private GsonUtils gsonUtils;
     private String[] taskNameList;
     public View view;
+
+    private NormalDialogUtil mapManageNormalDialog;
+
+    private List<ImageView> imageViewArrayList = new ArrayList<>();
+    private int index = 0;
+    double mBitmapHeight;
+    double mBitmapWidth;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -105,7 +121,6 @@ public class TaskManagerFragment extends Fragment implements View.OnClickListene
     }
 
     private void initListener() {
-
     }
 
     @Override
@@ -118,8 +133,8 @@ public class TaskManagerFragment extends Fragment implements View.OnClickListene
                         .addToBackStack(null)
                         .commit();
                 break;
+
             case R.id.task_manage_name:
-                gsonUtils.setMapName(Content.first_map_Name);//给上位机传入地图名称
                 MainActivity.emptyClient.send(gsonUtils.putJsonMessage(Content.GETTASKQUEUE));//请求任务列表
         }
 
@@ -138,6 +153,7 @@ public class TaskManagerFragment extends Fragment implements View.OnClickListene
         builder.create().show();
     }
 
+    @SuppressLint({"ResourceAsColor", "NewApi"})
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onEventMsg(EventBusMessage messageEvent){
         Log.d(TAG, "onEventMsg ： " + messageEvent.getState());
@@ -149,7 +165,113 @@ public class TaskManagerFragment extends Fragment implements View.OnClickListene
             bytes.get(bytes1);
             Log.d(TAG, "任务界面获得地图数据" + bytes1.length);
             Bitmap mBitmap = BitmapFactory.decodeByteArray(bytes1, 0, bytes1.length);
+            mBitmapHeight = mBitmap.getHeight();
+            mBitmapWidth = mBitmap.getWidth();
+            if (mBitmapHeight >= mBitmapWidth){
+                mBitmapWidth = taskManageMapRelativeBorder.getHeight()/mBitmapHeight*mBitmapWidth;
+                mBitmapHeight = taskManageMapRelativeBorder.getHeight();
+            }else if (mBitmapHeight > mBitmapWidth){
+                mBitmapHeight = taskManageMapRelativeBorder.getWidth()/mBitmapWidth*mBitmapHeight;
+                mBitmapWidth = taskManageMapRelativeBorder.getWidth();
+            }
+            MainActivity.emptyClient.send(gsonUtils.putJsonMessage(Content.GETPOINTPOSITION));
             taskManageMapImage.setImageBitmap(mBitmap);
+            taskManageMapRelative.setLayoutParams(new RelativeLayout.LayoutParams((int)mBitmapWidth,(int)mBitmapHeight));
+            RelativeLayout.LayoutParams layoutParams = new  RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,RelativeLayout.LayoutParams.MATCH_PARENT);
+            taskManageMapImage.setLayoutParams(layoutParams);
+            taskManageMapRelative.addView(taskManageMapImage);
+        } else if (messageEvent.getState() == 10008) {
+            for (int i = 0; i <imageViewArrayList.size(); i++) {
+                taskManageMapRelative.removeView(imageViewArrayList.get(i));
+            }
+            String message = (String) messageEvent.getT();
+            try {
+                JSONObject jsonObject = new JSONObject(message);
+                if (jsonObject != null) {
+                    JSONArray jsonArray = jsonObject.getJSONArray(Content.SENDPOINTPOSITION);
+                    Log.d("zdzd000 ", "pointName : " + jsonArray.toString());
+                    for (int k = 0; k < Content.list.size(); k++) {
+                        if (Content.list.get(k).getMap_Name().equals(Content.map_Name)) {
+                            Log.d("zdzd555", "" + Content.list.get(k).getResolution());
+                            index = k;
+                        }
+                    }
+                    Log.d("zdzd5111", "" + index);
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonItem = jsonArray.getJSONObject(i);
+                        Log.d("zdzd111 ", "pointName : " + jsonItem.toString());
+                        ImageView imageView = new ImageView(mContext);
+                        imageView.setImageResource(R.drawable.ic_point);
+                        imageViewArrayList.add(imageView);
+                        imageView.setOnClickListener(this);
+                        double gridHeight = Content.list.get(index).getGridHeight();
+                        double gridWidth = Content.list.get(index).getGridWidth();
+
+                        double pointX = jsonItem.getDouble(Content.POINT_X);
+                        int pointType = jsonItem.getInt(Content.POINT_TYPE);
+                        double pointY = jsonItem.getDouble(Content.POINT_Y);
+                        double originX = Content.list.get(index).getOriginX();
+                        double originY = Content.list.get(index).getOriginY();
+                        double resolution = Content.list.get(index).getResolution();
+                        double angleY = Math.sin(jsonItem.getDouble(Content.ANGLE));
+                        double angleX = Math.cos(jsonItem.getDouble(Content.ANGLE));
+
+                        Log.d("zdzd9998", "gridH"+gridHeight+"        gridW"+gridWidth + "     pointX"+pointX+"       originX"+originX+"       Content.ROBOT_SIZE "+Content.ROBOT_SIZE);
+                        Log.d("zdzd9998", " resolution * angleX"+  resolution*angleX);
+                        if (pointType == 2) {
+                            imageView.setPaddingRelative((int) (mBitmapWidth / gridWidth * (pointX - (Content.ROBOT_SIZE / resolution * angleX))),
+                                    (int) (mBitmapHeight - (mBitmapHeight / gridHeight * (pointY) - (Content.ROBOT_SIZE / resolution * angleY))),
+                                    0, 0);
+                            Log.d("zdzd9998", "angleX"+  angleX);
+                            Log.d("zdzd9998", " resolution"+  resolution);
+                            taskManageMapRelative.addView(imageView);
+                        }else if (pointType == 1){
+                        }
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        } else if (messageEvent.getState() == 40002) {//获取虚拟墙
+            String message = (String) messageEvent.getT();
+            double gridHeight = Content.list.get(index).getGridHeight();
+            double gridWidth = Content.list.get(index).getGridWidth();
+            double originX = Content.list.get(index).getOriginX();
+            double originY = Content.list.get(index).getOriginY();
+            try {
+                JSONObject jsonObject = new JSONObject(message);
+                JSONArray jsonArray = jsonObject.getJSONArray(Content.SEND_VIRTUAL);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    List<DrawLineBean> lineBeans = new ArrayList<>();
+                    for (int k = 0; k < jsonArray.getJSONArray(i).length(); k++) {
+                        JSONObject js = jsonArray.getJSONArray(i).getJSONObject(k);
+                        DrawLineBean drawLineBean = new DrawLineBean();
+                        drawLineBean.setX(js.getInt(Content.VIRTUAL_X));
+                        drawLineBean.setY(js.getInt(Content.VIRTUAL_Y));
+                        lineBeans.add(drawLineBean);
+                    }
+                    float startX = 0, startY = 0, endX = 0, endY = 0;
+                    for (int k = 0; k < lineBeans.size(); k++) {
+                        if (k == 0) {
+                            startX = (float) ((mBitmapWidth / (lineBeans.get(k).getX() * gridWidth)) + originX);
+                            startY = (float) (mBitmapHeight / (gridHeight * (mBitmapHeight - lineBeans.get(k).getY())) + originY);
+                        } else {
+                            endX = (float) ((mBitmapWidth / (lineBeans.get(k).getX() * gridWidth)) + originX);
+                            endY = (float) (mBitmapHeight / (gridHeight * (mBitmapHeight - lineBeans.get(k).getY())) + originY);
+                        }
+                    }
+                    //划线：点连线 连接start和end
+                    Canvas canvas = new Canvas();
+                    Paint paint = new Paint();
+                    paint.setColor(R.color.colorPrimaryDark);
+                    canvas.drawLine(startX, startY, endX, endY, paint);
+                    taskManageMapRelative.draw(canvas);
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }else if(messageEvent.getState() == 10017){
             taskNameList = (String[]) messageEvent.getT();
             Log.d("task_name",taskNameList[0]);
